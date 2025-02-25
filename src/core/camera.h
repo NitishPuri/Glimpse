ccb360da70da7d14d7f7e2652c0cd995e7e45df8
Nitish Puri <npuri1903@gmail.com>
2025-02-25 17:53:45 +0530

    defocus blur++

--- src/core/camera.h
+++ src/core/camera.h
@@ -13,6 +13,9 @@
 // 6. The camera fires rays from random points on the lens
 //    through the current image sample.
 
+#define DEFOCUS_IMPL 1
+// Options :: 0 = isotropic blur, 1 = anisotropic defocus blur
+
 class camera {
  public:
   void initialize() {
@@ -33,26 +36,53 @@ class camera {
     u = unit_vector(cross(vup, w));
     v = cross(w, u);
 
+    // Calculate the vectors across the horizontal and down the vertical
+    // viewport edges.
     horizontal = viewport_width * u;
-    vertical = viewport_height * v;
+    vertical = viewport_height * v;  // TODO: should this be inverted ?
 
     lower_left_corner =
         origin - horizontal / 2 - vertical / 2 - focus_distance * w;
 
+#if DEFOCUS_IMPL == 0
     lens_radius = aperture / 2;
+#else
+    // Calculate the camera defocus disk basis vectors.
+    defocus_angle = aperture;
+    auto defocus_radius =
+        focus_distance * std::tan(degrees_to_radians(defocus_angle / 2));
+    defocus_disk_u = u * defocus_radius;
+    defocus_disk_v = v * defocus_radius;
+#endif
   }
 
+  // normalised coordinates!
   ray get_ray(double s, double t) const {
     // without defocus blur, all rays pass through the origin
+
+#if DEFOCUS_IMPL == 0
     vec3 rd = lens_radius * random_in_unit_disk();
     vec3 offset = u * rd.x() + v * rd.y();
 
     auto ray_origin = origin + offset;
+#else
+    auto ray_origin = (defocus_angle <= 0) ? origin : defocus_disk_sample();
+#endif
+
     auto ray_direction =
         lower_left_corner + s * horizontal + t * vertical - ray_origin;
+
     // Random time for motion blur?
     return ray(ray_origin, ray_direction, random_double(time0, time1));
   }
+
+#if DEFOCUS_IMPL == 1
+  point3 defocus_disk_sample() const {
+    // Returns a random point in the camera defocus disk.
+    auto p = random_in_unit_disk();
+    return origin + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+  }
+#endif
 
  public:
   double aspect_ratio = 16.0 / 9.0;  // Ratio of image width over height
@@ -77,9 +107,16 @@ class camera {
 
  private:
   point3 origin;
+
   point3 lower_left_corner;
   vec3 horizontal;
   vec3 vertical;
+
   vec3 u, v, w;
+
+#if DEFOCUS_IMPL == 0
   double lens_radius;
+#elif DEFOCUS_IMPL == 1
+  vec3 defocus_disk_u, defocus_disk_v;
+#endif
 };
