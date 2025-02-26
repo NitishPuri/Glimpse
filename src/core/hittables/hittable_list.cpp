df2ea301b655a837295d2a8f2ae3624a5835dfa9
Nitish Puri <npuri1903@gmail.com>
2025-02-27 01:07:38 +0530

    hittable list

--- src/core/hittables/hittable_list.cpp
+++ src/core/hittables/hittable_list.cpp
@@ -1,18 +1,18 @@
-#include "hittable_list.h"
+// #include "hittable_list.h"
 
-bool hittable_list::hit(const ray& r, const interval& ray_t,
-                        hit_record& rec) const {
-  hit_record temp_rec;
-  bool hit_anything = false;
-  auto closest_so_far = ray_t.max;
+// bool hittable_list::hit(const ray& r, const interval& ray_t,
+//                         hit_record& rec) const {
+//   hit_record temp_rec;
+//   bool hit_anything = false;
+//   auto closest_so_far = ray_t.max;
 
-  for (const auto& object : objects) {
-    if (object->hit(r, {ray_t.min, closest_so_far}, temp_rec)) {
-      hit_anything = true;
-      closest_so_far = temp_rec.t;
-      rec = temp_rec;
-    }
-  }
+//   for (const auto& object : objects) {
+//     if (object->hit(r, {ray_t.min, closest_so_far}, temp_rec)) {
+//       hit_anything = true;
+//       closest_so_far = temp_rec.t;
+//       rec = temp_rec;
+//     }
+//   }
 
-  return hit_anything;
-}
+//   return hit_anything;
+// }
