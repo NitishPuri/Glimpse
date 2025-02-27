b0de2949a33974856f04f375b2639b230c223a2e
Nitish Puri <npuri1903@gmail.com>
2025-02-27 12:33:22 +0530

    ff

--- src/core/hittables/constant_medium.h
+++ src/core/hittables/constant_medium.h
@@ -1,15 +1,4 @@
-#ifndef CONSTANT_MEDIUM_H
-#define CONSTANT_MEDIUM_H
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
+#pragma once
 
 #include "hittable.h"
 #include "material.h"
@@ -60,5 +49,3 @@ class constant_medium : public hittable {
   double neg_inv_density;
   shared_ptr<material> phase_function;
 };
-
-#endif
