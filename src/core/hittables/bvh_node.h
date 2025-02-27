6aa1e3c5d556c7fdedb7b58d017690d0343fd38c
Nitish Puri <npuri1903@gmail.com>
2025-02-27 12:25:08 +0530

    bvh

--- src/core/hittables/bvh_node.h
+++ src/core/hittables/bvh_node.h
@@ -15,40 +15,9 @@ class bvh_node : public hittable {
     // persist the resulting bounding volume hierarchy.
   }
 
-  bvh_node(std::vector<shared_ptr<hittable>>& objects, size_t start, size_t end) {
-    // Build the bounding box of the span of source objects.
-    bbox = aabb::empty;
-    for (size_t object_index = start; object_index < end; object_index++)
-      bbox = aabb(bbox, objects[object_index]->bounding_box());
+  bvh_node(std::vector<shared_ptr<hittable>>& objects, size_t start, size_t end);
 
-    int axis = bbox.longest_axis();
-
-    auto comparator = (axis == 0) ? box_x_compare : (axis == 1) ? box_y_compare : box_z_compare;
-
-    size_t object_span = end - start;
-
-    if (object_span == 1) {
-      left = right = objects[start];
-    } else if (object_span == 2) {
-      left = objects[start];
-      right = objects[start + 1];
-    } else {
-      std::sort(std::begin(objects) + start, std::begin(objects) + end, comparator);
-
-      auto mid = start + object_span / 2;
-      left = make_shared<bvh_node>(objects, start, mid);
-      right = make_shared<bvh_node>(objects, mid, end);
-    }
-  }
-
-  bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-    if (!bbox.hit(r, ray_t)) return false;
-
-    bool hit_left = left->hit(r, ray_t, rec);
-    bool hit_right = right->hit(r, interval(ray_t.min, hit_left ? rec.t : ray_t.max), rec);
-
-    return hit_left || hit_right;
-  }
+  bool hit(const ray& r, interval ray_t, hit_record& rec) const override;
 
   aabb bounding_box() const override { return bbox; }
 
